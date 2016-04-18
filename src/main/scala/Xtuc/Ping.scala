49912ff6c479633a5c72c4c8df575f818957bdb6
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-04-18 16:43:09 +0200

    Moved ping classes in new file

---
+++ src/main/scala/Xtuc/Ping.scala
@@ -0,0 +1,17 @@
+package Xtuc
+
+import akka.actor.ActorRef
+
+case class MakePing(address: String, portFrom: Int, portTo: Int, PingSender: ActorRef, cache: ActorRef)
+
+case class Ping(ipv4: String, port: Int, timeout: Int = 50) extends Ordered[Ping] {
+
+  def compare(that: Ping): Int = that match {
+    case Ping(h, p, _) =>
+      if(ipv4.length > h.length) 1
+      else
+      if (port > p) 1
+      else -1
+    case _ => 0
+  }
+}
